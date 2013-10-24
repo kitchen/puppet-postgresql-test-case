6926ade6a7f3c650e71299322e61faab9ad977d4
Jeremy Kitchen <kitchen@kitchen.io>
2013-10-24 21:46:01 +0000

    since this is a catalog fail, it doesn't really need sudo

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ set -x
 
 bundle install
 bundle exec librarian-puppet install
-sudo bundle exec puppet apply --hiera_config=hiera.yaml --modulepath=modules manifest.pp
+bundle exec puppet apply --hiera_config=hiera.yaml --modulepath=modules manifest.pp
